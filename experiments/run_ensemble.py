19734794890321902e052ef2bca67a31f5e5c4ce
danielplatt <d.platt@web.de>
2023-03-02 14:09:32 +0000

    Add file that runs full training suite

---
+++ experiments/run_ensemble.py
@@ -0,0 +1,30 @@
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+from models.BullHeJejjalaMishraCNN import train_bull_he_jejjala_mishra_network
+from models.ErbinFinotelloInception import train_erbin_finotello
+from models.Hartford import train_hartford_network
+from models.HeNN import train_he_network
+
+from data.load_data import load_data
+
+
+def training(trainingtask, datasetpreprocessing='', datasetpermuted=False):
+    data = load_data(datasetpreprocessing, datasetpermuted)
+    X_train, X_test, y_train, y_test = train_test_split(np.array(data[0]), np.array(data[1])[:, 0], test_size=0.5)
+    print(f'Beginning training task {trainingtask}')
+    print(f'Train on dataset with preprocessing "{datasetpreprocessing}"')
+    print(f'Has dataset been permuted before pre-processing? {datasetpermuted}')
+    accuracy_result = trainingtask(X_train, y_train, X_test, y_test)
+    return accuracy_result
+
+def main():
+    for trainingtask in [train_bull_he_jejjala_mishra_network, train_erbin_finotello, train_hartford_network, train_he_network]:
+        for datasetpreprocessing in ['', 'dirichlet', 'combinatorial']:
+            for datasetpermuted in [False, True]:
+                training(trainingtask, datasetpreprocessing, datasetpermuted)
+                print('---')
+
+
+if __name__ == '__main__':
+    main()
